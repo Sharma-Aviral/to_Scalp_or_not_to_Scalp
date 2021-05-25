df44bac1bfdcff6c2b9e6cf1132e2d6ce952d7a8
unknown <aviral.sharma.012@gmail.com>
2021-05-25 21:39:58 +0530

    getting ps5

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('https://rptechindia.in/nvidia-geforce-rtx-3060-ti.html')
+browser.get('https://www.flipkart.com/sony-playstation-5-cfi-1008a01r-825-gb-astro-s-playroom/p/itma0201bdea62fa')
+element = browser.find_element_by_class_name("_1INA6Y _1MBTtW")
+print(element)
