fc37b0c003382678f85dc90ec3e016c7423aa50d
unknown <aviral.sharma.012@gmail.com>
2021-05-26 21:21:32 +0530

    Ready to scalp

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
-
 browser = webdriver.Firefox()
-browser.get('https://www.flipkart.com/sony-playstation-5-cfi-1008a01r-825-gb-astro-s-playroom/p/itma0201bdea62fa')
-element = browser.find_element_by_class_name("_1INA6Y _1MBTtW")
-print(element)
+browser.get('https://www.flipkart.com/sony-chu-2208bb01i-1000-gb-spider-man-ratchet-clank-gran-turismo/p/itm8230ffd9a827b?pid=GMCFUAKAMHX6QJ2C&lid=LSTGMCFUAKAMHX6QJ2C1HTDLX&marketplace=FLIPKART&q=ps4&store=4rr&srno=s_1_1&otracker=search&otracker1=search&fm=SEARCH&iid=ce6379e2-5ac3-4f4a-a336-1d1bce1256fb.GMCFUAKAMHX6QJ2C.SEARCH&ppt=browse&ppn=browse&ssid=toxzlozlu5xmtslc1622043007335&qH=0e740342f31e9aa6') 
+notCLickable = True
+# passwordBox = browser.find_element_by_class_name("_2IX_2-._3mctLh.VJZDxU")
+# userName  = browser.find_element_by_class_name("_2IX_2-.VJZDxU")
+while notCLickable :
+    browser.refresh()
+      
+    try:
+        element = browser.find_element_by_class_name("_2KpZ6l._2U9uOA.ihZ75k._3AWRsL")
+        element.click()
+        print("added _to_cart")
+        notCLickable = False
+    except:
+        print("Refreshing")    
+
+    print("dedeya")     
